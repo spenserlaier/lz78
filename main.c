b44427b8ad939bb5465bfbc1e8cbd09ac0eed6d2
Spenser Laier <spenserlaier@gmail.com>
2024-12-31 02:17:38 -0500

    resolve bugs

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 
 const int STR_TABLE_SIZE = 4096;
+const int MAX_TABLE_IDX = (2<<15) - 1;
 
 
 unsigned short find_str_in_table(char * s, char * table[]) {
@@ -14,6 +15,7 @@ unsigned short find_str_in_table(char * s, char * table[]) {
             return -1;
         }
         if (strcmp(s, table[i]) == 0) {
+            printf("successful comparison: %s and %s\n", s, table[i]);
             return i;
         }
     }
@@ -34,6 +36,7 @@ int initialize_table(char * table[]) {
 
 
 int lz78_encode(char * input_file_path, char * output_file_path) {
+    printf("%d\n", MAX_TABLE_IDX);
     char * string_table[STR_TABLE_SIZE];
     int initialize_result = initialize_table(string_table);
     if (initialize_result == -1) {
@@ -61,7 +64,8 @@ int lz78_encode(char * input_file_path, char * output_file_path) {
 
     curr_word[0] = '\0';
     size_t curr_word_size = 1;
-    unsigned short prev_idx = -1;
+    unsigned short prev_idx = MAX_TABLE_IDX;
+    //TODO: it's unsigned, setting to -1 doesn't work.
     while ((file_char = fgetc(input)) != EOF) {
         //printf("current character: %c\n", file_char);
         char tmp[2];
@@ -77,20 +81,29 @@ int lz78_encode(char * input_file_path, char * output_file_path) {
         //printf("done. searching for string in table\n");
         int str_table_search_idx = find_str_in_table(curr_word, string_table);
         //printf("done: idx is %d\n", str_table_search_idx);
-        if (str_table_search_idx != -1) {
-            printf("Found match in table for string: %s\n", curr_word);
+        if (str_table_search_idx == 0) {
+            //printf("Found match in table for string: %s\n", curr_word);
             //set the prev index into the table for use later if needed
             prev_idx = str_table_search_idx;
+            printf("prev_idx set to %d\n", prev_idx);
             //nothing else to do
         } else {
-            printf("String not found in table: %s\n", curr_word);
+            //printf("String not found in table: %s\n", curr_word);
             //add curr word (excluding new char) to the table
             curr_word[curr_word_size-2] = '\0'; // reset the character we just added
             char * copied_word = malloc(sizeof(char)* curr_word_buff_size);
             size_t copy_len = strlcpy(copied_word, curr_word, curr_word_buff_size);
             string_table[string_table_idx++] = copied_word;
             //emit the code of the last word used into the output file stream
-            fwrite(&prev_idx, sizeof(prev_idx), 1, output);
+            printf("here's the value of the code we wrote: %d\n", prev_idx);
+            if ( prev_idx != MAX_TABLE_IDX) {
+                printf("writing the code: %d\n", prev_idx);
+                fwrite(&prev_idx, sizeof(prev_idx), 1, output);
+            } else {
+                printf("defaulting to regular character code: %c\n", tmp[0]);
+                unsigned short padded_char = tmp[0];
+                fwrite(&padded_char, sizeof(unsigned short), 1, output);
+            }
             //set current_word to the latest character found
             curr_word[0] = tmp[0];
             curr_word[1] = '\0';
@@ -100,14 +113,13 @@ int lz78_encode(char * input_file_path, char * output_file_path) {
     }
 
     fclose(input);
-    fprintf(output, "Hello, World!\n");
-    fprintf(output, "This is a test file.\n");
     fclose(output);
     //TODO: free memory
     return 0;
 }
 
 int lz78_decode(char * input, char* output) {
+    printf("beginning decoding \n");
     FILE *encoded_file = fopen(input, "r");
     if (encoded_file == NULL) {
         perror("Error opening file");
@@ -126,17 +138,21 @@ int lz78_decode(char * input, char* output) {
     char * curr_word = malloc(sizeof(char)*curr_word_buff_size);
     curr_word[0] = '\0';
     size_t curr_word_size = 1;
+    printf("mark\n");
     while ((bytesRead = fread(&code, sizeof(code), 1, encoded_file)) == 1) {
         //first, write the stored value of the code to the output stream
+        printf("just after mark\n");
+        printf("current code: %d\n", code);
         fputs(string_table[code], decoded_file);
         int idx = 0;
+        printf("current code: %d\n", code);
         while (string_table[code][idx] != '\0') {
             curr_word_size ++;
             if (curr_word_size > curr_word_buff_size) {
                 curr_word = realloc(curr_word, curr_word_size);
             }
             int str_table_search_idx = find_str_in_table(curr_word, string_table);
-            if (str_table_search_idx != -1) {
+            if (str_table_search_idx == 0) {
                 //nothing to do
             } else {
                 //the entry doesn't exist; update the code for the word, excluding the current
@@ -168,7 +184,6 @@ int lz78_decode(char * input, char* output) {
 
 
 int main() {
-    lz78_encode("./input.txt", "./output.txt");
-
-
+    lz78_encode("./raw.txt", "./encoded.txt");
+    lz78_decode("./encoded.txt", "./decoded.txt");
 }
